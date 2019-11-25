69501bee3d228a5a2e37686dee00ba1d55ce3d99
rdkerr <rogerdkerr@gmail.com>
2019-11-24 18:28:43 -0700

    Eslint setup, removed css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+`;
+
+const AppHeader = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+// .App-link {
+//   color: #09d3ac;
+// }
+
+
+const App: React.FC = () => (
+  <AppContainer>
+    <AppHeader>
+      <AppLogo src={logo} alt="logo" />
+    </AppHeader>
+  </AppContainer>
+);
 
 export default App;
