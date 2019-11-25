36482bef846b4faf7d3f3230aa6745d85c77b127
rdkerr <rogerdkerr@gmail.com>
2019-11-24 19:28:44 -0700

    Display basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const AppLogo = styled.img`
 
 const AppHeader = styled.div`
   background-color: #282c34;
-  min-height: 100vh;
+  min-height: 10vh;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -25,12 +25,36 @@ const AppHeader = styled.div`
 //   color: #09d3ac;
 // }
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 10px;
+  background-color: #AAA;
+`;
+
+const Tile = styled.div`
+  background-color: #444;
+  color: #FFF;
+  padding: 20px;
+  font-size: 150%;
+  border: 1px solid #222;
+`;
+
+const BOARD = Array.from({ length: 5 }, () => (
+  Array.from({ length: 5 }, () => true)
+));
+
+let key = 0;
 
 const App: React.FC = () => (
+
   <AppContainer>
     <AppHeader>
       <AppLogo src={logo} alt="logo" />
     </AppHeader>
+    <Grid>
+      {BOARD.map((row, i) => row.map((el, j) => <Tile key={key++}>{el ? 1 : 0}</Tile>))}
+    </Grid>
   </AppContainer>
 );
 
