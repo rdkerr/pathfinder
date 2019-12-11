a3b28c4736fddc251a109c5dd26711855875b5f0
rdkerr <rogerdkerr@gmail.com>
2019-12-10 18:18:54 -0700

    refactor: add background animation

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+*{
+  transition: background-color 0.5s ease-in-out;
+}
   body {
     margin: 0;
     padding: 0;
     height: 100vh;
-    transition: all 0.25s linear;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
